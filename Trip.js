2ead50a3e0135c74e1c2f7c976628bf4bf8d605a
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-19 11:01:36 +0530

    changes

--- Trip.js
+++ Trip.js
@@ -22,3 +22,38 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+
+
+
+function validateForm(event) {
+  event.preventDefault();
+  const form = document.getElementById('holidayForm');
+  const fields = ['departing', 'nights', 'destination', 'people', 'leaving'];
+  let valid = true;fields.forEach(field => {
+const input = document.getElementById(field);
+if (!input.value) {
+  input.style.borderColor = 'red';
+  valid = false;
+} else {
+  input.style.borderColor = '#ccc';
+}
+});
+
+if (valid) {
+  Swal.fire({
+    title: "Holidays serach succesfully",
+    icon: "success",
+    draggable: true
+
+  });
+form.reset();
+} else {
+  Swal.fire({
+    icon: "error",
+    title: "Incomplete Form",
+    text: "Please fill all the fields before booking!",
+    confirmButtonColor: "#ff0000"
+});
+}
+}
